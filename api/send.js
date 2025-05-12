7c5e345985a0d8e0d62ec99b1dad6559b09ddfa7
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2025-05-12 23:48:43 +0500

    modified code as per esp

--- api/send.js
+++ api/send.js
@@ -8,20 +8,29 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
-const sensorSchema = new mongoose.Schema({
-  temperature: Number,
-  humidity: Number,
-  timestamp: { type: Date, default: Date.now }
+const SensorDataSchema = new mongoose.Schema({
+  temperature: {
+    type: Number,
+    required: true
+  },
+  humidity: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Sensor = mongoose.model("Sensor", sensorSchema);
+const Sensor = mongoose.model("SensorData", SensorDataSchema);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       // Save the sensor data
-      const sensorData = new Sensor(req.body);
-      await sensorData.save();
+      const sensor_data = new SensorData(req.body);
+      await sensor_data.save();
       res.status(200).send("Data saved");
     } catch (err) {
       res.status(500).send("Error: " + err.message);
